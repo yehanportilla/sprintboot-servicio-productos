f213312967932ac0242dc2e2384c680317a03766
yehanportilla <jancha22@gmail.com>
2019-09-12 15:56:18 -0500

    creacion proyecto

---
+++ src/test/java/com/formacionbdi/sprintboot/app/productos/SprintbootServicioProductosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.sprintboot.app.productos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprintbootServicioProductosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
