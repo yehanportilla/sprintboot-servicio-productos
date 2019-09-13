77e5c759d6809605fc39d814f6b4613ff40e33cc
yehanportilla <jancha22@gmail.com>
2019-09-13 10:27:29 -0500

    add class ProductoServiceImpl e interface IProductoService

---
+++ src/main/java/com/formacionbdi/sprintboot/app/productos/models/service/IProductoService.java
@@ -0,0 +1,18 @@
+package com.formacionbdi.sprintboot.app.productos.models.service;
+
+import java.util.List;
+
+import com.formacionbdi.sprintboot.app.productos.models.entity.Producto;
+
+/**
+ * 
+ * @author yehan.portilla
+ *
+ */
+public interface IProductoService {
+	
+	public List<Producto> findAll();
+	
+	public Producto finById(Long id);
+	
+}
---
+++ src/main/java/com/formacionbdi/sprintboot/app/productos/models/service/ProductoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.formacionbdi.sprintboot.app.productos.models.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.formacionbdi.sprintboot.app.productos.models.dao.ProductoDao;
+import com.formacionbdi.sprintboot.app.productos.models.entity.Producto;
+
+/**
+ * 
+ * @author yehan.portilla
+ *
+ */
+@Service
+public class ProductoServiceImpl implements IProductoService {
+
+	@Autowired
+	private ProductoDao productoDao;
+
+	/**
+	 * Method in charge of listing all products
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<Producto> findAll() {
+
+		return (List<Producto>) productoDao.findAll();
+	}
+
+	/**
+	 * method of searching for product by id
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Producto finById(Long id) {
+
+		return productoDao.findById(id).orElse(null);
+	}
+
+}
